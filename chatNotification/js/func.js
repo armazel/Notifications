6b4fbfd337045b4a6b66f32e32d5cf0863413514
armazel <dragun.egor@gmail.com>
2016-01-17 20:30:56 +0200

    Реализована задача нотификаций.

---
+++ chatNotification/js/func.js
@@ -0,0 +1,13 @@
+var lis = document.querySelectorAll('.twitt li');
+
+
+    $( "#button" ).each(function() {
+        $(this).bind("click", function () {
+
+            var li = document.forms['idform'].elements['id'].value;
+            if (li != '') {
+                $('.notifications').append('<li class="twitt"> <img src="https://pp.vk.me/c6060/v6060369/153d2/z5sF-fiVOi0.jpg" width="30px" height="30px">' + " " + li + '<a href="#" onclick="var li = this.parentNode; var ul = li.parentNode; ul.removeChild(li)"; ><img src="http://www.iconsearch.ru/uploads/icons/fatcow/32x32/cross.png" style="float: right" class="Cross"></a></li>');
+            }
+        });
+
+    });
