751c51ed2c5a3bc121371408597484f84d326e5b
Egor <dragun.egor@gmail.com>
2016-01-29 21:26:26 +0200

    Update func.js

--- chatNotification/js/func.js
+++ chatNotification/js/func.js
@@ -1,11 +1,17 @@
-
-    $( "#button" ).each(function() {
+$( "#button" ).each(function() {
         $(this).bind("click", function () {
 
             var li = document.forms['idform'].elements['id'].value;
             if (li != '') {
-                $('.notifications').append('<li class="twitt"> <img src="https://pp.vk.me/c6060/v6060369/153d2/z5sF-fiVOi0.jpg" width="30px" height="30px">' + " " + li + '<a href="#" onclick="var li = this.parentNode; var ul = li.parentNode; ul.removeChild(li)"; ><img src="http://www.iconsearch.ru/uploads/icons/fatcow/32x32/cross.png" style="float: right" class="Cross"></a></li>');
+               var add= $('.notifications').append('<li class="twitt"> <img src="https://pp.vk.me/c6060/v6060369/153d2/z5sF-fiVOi0.jpg" width="30px" height="30px">' + " " + li + '<a href="#" class="removed" ><img src="http://www.iconsearch.ru/uploads/icons/fatcow/32x32/cross.png" style="float: right" class="Cross"></a></li>');
+                var cross=document.getElementsByClassName("removed");
             }
+                return cross.onclick=function(add){
+                    add.remove("li.removed");
+                }
         });
 
     });
+
+
+
