0f4650f2a10412f75565558501563fd69860a538
Egor <dragun.egor@gmail.com>
2016-02-27 14:36:41 +0200

    Update func.js

--- chatNotification/js/func.js
+++ chatNotification/js/func.js
@@ -1,33 +1,23 @@
+$( "#button" ).each(function() {
+    $(this).bind("click", function () {
 
+        var li = document.forms['idform'].elements['id'].value;
+        if (li != '') {
+            var add=$('<li class="twitt"> <img src="https://pp.vk.me/c6060/v6060369/153d2/z5sF-fiVOi0.jpg" width="30px" height="30px">' + " " + li + '<a href="#" class="removed" ><img src="http://www.iconsearch.ru/uploads/icons/fatcow/32x32/cross.png" style="float: right" class="cross"></a></li>');
+            $('.notifications').append(add);
 
-
-
-
-    $( "#button" ).each(function() {
-        $(this).bind("click", function () {
-
-            var li = document.forms['idform'].elements['id'].value;
-            if (li != '') {
-               var add= $('.notifications').append('<li class="twitt"> <img src="https://pp.vk.me/c6060/v6060369/153d2/z5sF-fiVOi0.jpg" width="30px" height="30px">' + " " + li + '<a href="#" class="removed" ><img src="http://www.iconsearch.ru/uploads/icons/fatcow/32x32/cross.png" style="float: right" class="Cross"></a></li>');
-                        var cross=document.getElementsByClassName("removed");
-                var liArray = document.getElementsByClassName('twitt');
-                var liContainArr = [];
-                for(var i=0; i<liArray.length; i++){
-                    liContainArr[i] = liArray[i];
-                }
-                for(var i=0; i<liContainArr.length;i++){
-                    (function(i){
-                        liContainArr[i].onclick=function(){
-                            liContainArr[i].remove();
-
-                        }
-                    })(i);
-                }
-            }
-
-        });
+           add.find('.cross').click(function(){
+              add.remove();
+           });
+        }
 
     });
 
+});
 
+
+
+
+
+   
 
